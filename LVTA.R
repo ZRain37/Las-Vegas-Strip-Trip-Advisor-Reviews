de3ea79a9f537c764ea32455334416b861c51c08
Zachary Rainwater <65995991+ZRain37@users.noreply.github.com>
2020-11-29 22:52:30 -0500

    Add files via upload

---
+++ LVTA.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(readr)
+library(Hmisc)
+LVTA <- read_csv("Downloads/LasVegasTripAdvisorReviews-Dataset.csv", na = "NA")
+LVTA <- na.omit(LVTA)
+View(LVTA)
+summary(LVTA)
+names(LVTA)
+length(is.na(LVTA))
+
+
+ggplot(data = LVTA) +
+  geom_bar(aes(x=Hotel.name, y=Score),
+           stat = "summary", fun.y <- "mean", color = "blue")
